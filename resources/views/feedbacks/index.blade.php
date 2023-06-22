6c4a352037de4cc512bce26ad5b7d7966b17bdfa
Zaenab <zaenab.1701@gmail.com>
2023-06-23 01:46:47 +0800

    +blade show feedback & relawan

--- resources/views/feedbacks/index.blade.php
+++ resources/views/feedbacks/index.blade.php
@@ -1,31 +1,48 @@
 @extends('layouts.app')
 @section('content')
+    <div class="content-header row">
+        <div class="content-header-left col-md-6 col-12 mb-1">
+        <h2 class="content-header-title">Feedback</h2>
+        </div>
+        <div class="content-header-right breadcrumbs-right breadcrumbs-top col-md-6 col-12">
+        <div class="breadcrumb-wrapper col-12">
+            <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#">Dashboard</a>
+            </li>
+            {{-- <li class="breadcrumb-item"><a href="#"></a>
+            </li> --}}
+            <li class="breadcrumb-item active">Feedback
+            </li>
+            </ol>
+        </div>
+        </div>
+    </div>
     <div class="content-body">
         <div class="row">
             <div class="col-12">
                 @include('flash::message')
                 <div class="clearfix"></div>
                 <div class="card">
-                    <div class="border-light-blue border-left-6 box-shadow-1 rounded">
+                    <div class="border-green border-left-6 box-shadow-1 rounded">
                         <div class="card-content ">
                             @if ($feedbackAbility)
                                 <form action="/feedback" method="POST" class="p-3">
                                     @csrf
                                     <div class="mb-3">
-                                        <label for="judulFeedback" class="form-label">Judul Feedback</label>
-                                        <input type="text" name="judulFeedback" class="form-control" id="judulFeedback">
+                                        <label for="judulFeedback" class="form-label">Judul</label>
+                                        <input type="text" name="judulFeedback" class="form-control border-light-green border-darken-4 border-left-6" id="judulFeedback">
                                     </div>
                                     <div class="mb-3">
                                         <label for="feedback"  class="form-label">Feedback</label>
-                                        <textarea class="form-control" name="feedback" id="feedback" rows="3"></textarea>
+                                        <textarea class="form-control border-light-green border-darken-4 border-left-6" name="feedback" id="feedback" rows="3"></textarea>
                                     </div>
                                     
-                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                    <button type="submit" class="btn btn-green">Kirim</button>
 
                                 </form>
                             @else
                                 <h2 class="mx-5 my-3">Feedback anda telah kami terima.</h2>
-                                <h6 class="mx-5 my-3"> Anda Bisa mengirimkan feedback 7 hari sekali.</h6>
+                                <h6 class="mx-5 my-3"> Anda Bisa mengirimkan feedback setelah 7 hari.</h6>
                             @endif
                             
                         </div>
--- resources/views/relawans/show_fields.blade.php
+++ resources/views/relawans/show_fields.blade.php
@@ -1,77 +1,98 @@
-<!-- Nama Field -->
 <div class="row">
-    <div class="col-sm-4">
-        {!! Form::label('nama', 'Nama:') !!}
-        <p>{{ $relawan->users->name }}</p>
+    <div class="col-xl-3 col-md-6 col-6">
+        <div class="card">
+            <div class="text-center">
+                <div class="card-body">
+                    @if(empty(Auth::user()->foto))
+                    <img src="{{ asset('image/avatar.png') }}" alt="avatar" class="rounded-circle  height-150" alt="Card image"><i></i>
+                    @else
+                        <img src="{{ asset(Auth::user()->foto) }}" alt="avatar" class="rounded-circle  height-150" alt="Card image" ><i></i>
+                    @endif
+                </div>
+                <div class="card-body">
+                    <h4 class="card-title">{{ $relawan->users->name }}</h4>
+                    <h6 class="card-subtitle text-muted">{{ isset($relawan->desa->nama)?$relawan->desa->nama:""}}</h6>
+                </div>
+                <div class="text-center">
+                    <a href="#" class="btn btn-social-icon mr-1 mb-1 btn-outline-github"><span class=" fa fa-phone"></span></a>
+                    <a href="#" class="btn btn-social-icon mr-1 mb-1 btn-outline-twitter"><span class="fa fa-envelope font-medium-4"></span></a>
+                    <a href="#" class="btn btn-social-icon mb-1 btn-outline-google"><span class="fa fa-map-marker"></span></a>
+                </div>
+            </div>
+        </div>
     </div>
-    <!-- Relawan ID Field -->
-    <div class="col-sm-4">
-        {!! Form::label('relawan_id', 'Id Relawan:') !!}
-        <p>{{ $relawan->id }}</p>
+    <div class="col-xl-9 col-md-6 col-6">
+        <div class="row">
+                <!-- Nama Field -->
+            <div class="col-xl-3 col-md-6 col-6">
+                {!! Form::label('nama', 'Nama:') !!}
+                <p class="font-weight-bold">{{ $relawan->users->name }}</p>
+            </div>
+            <!-- Relawan ID Field -->
+            <div class="col-xl-3 col-md-6 col-6">
+                {!! Form::label('relawan_id', 'Id Relawan:') !!}
+                <p class="font-weight-bold">{{ $relawan->id }}</p>
+            </div>
+            <!-- Status Relawan Field -->
+            <div class="col-xl-3 col-md-6 col-6">
+                {!! Form::label('status', 'Status Relawan:') !!}
+                <p class="font-weight-bold">{{ $relawan->status }}</p>
+            </div>
+            <!-- Desa Field -->
+            <div class="col-xl-3 col-md-6 col-6">
+                {!! Form::label('id_wilayah', 'Desa:') !!}
+                <p class="font-weight-bold">{{ isset($relawan->desa->nama)?$relawan->desa->nama:""}}</p>
+            </div>
+        </div>
+        <div class="row">
+            <!-- Kecamatan Field -->
+            <div class="col-xl-3 col-md-6 col-6">
+                {!! Form::label('id_wilayah', 'Kecamatan:') !!}
+                <p class="font-weight-bold">{{ isset($relawan->desa->kecamatan->nama)?$relawan->desa->kecamatan->nama:""}}</p>
+            </div>
+            <!-- No KTA Field -->
+            <div class="col-xl-3 col-md-6 col-6">
+                {!! Form::label('no_kta', 'No KTA:') !!}
+                <p class="font-weight-bold">{{ $relawan->no_kta }}</p>
+            </div>
+            <!-- NIK Field -->
+            <div class="col-xl-3 col-md-6 col-6">
+                {!! Form::label('nik', 'NIK:') !!}
+                <p class="font-weight-bold">{{ $relawan->nik }}</p>
+            </div>
+            <!-- Jenis Kelamin Field -->
+            <div class="col-xl-3 col-md-6 col-6">
+                {!! Form::label('jenis_kelamin', 'Jenis Kelamin:') !!}
+                <p class="font-weight-bold">{{ $relawan->jenis_kelamin}}</p>
+            </div>
+        </div>
+        <div class="row">
+            <!-- Tempat Lahir Field -->
+            <div class="col-xl-3 col-md-6 col-6">
+                {!! Form::label('tempat_lahir', 'Tempat Lahir:') !!}
+                <p class="font-weight-bold">{{ $relawan->tempat_lahir}}</p>
+            </div>
+            <!-- Tanggal Lahir Field -->
+            <div class="col-xl-3 col-md-6 col-6">
+                {!! Form::label('tanggal_lahir', 'Tanggal Lahir:') !!}
+                <p class="font-weight-bold">{{ $relawan->tanggal_lahir}}</p>
+            </div>
+            <!-- Status Perkawinan Field -->
+            <div class="col-xl-3 col-md-6 col-6">
+                {!! Form::label('status_perkawinan', 'Status Perkawinan:') !!}
+                <p class="font-weight-bold">{{ $relawan->status_perkawinan}}</p>
+            </div>
+            <!-- Kontak Field -->
+            <div class="col-xl-3 col-md-6 col-6">
+                {!! Form::label('contact', 'Kontak:') !!}
+                <p class="font-weight-bold">{{ $relawan->users->contact}}</p>
+            </div>
+        </div>
+        <!-- Alamat Field -->
+        <div class="col-12">
+            {!! Form::label('alamat', 'Alamat:') !!}
+            <p class="font-weight-bold">{{ $relawan->users->alamat}}</p>
+        </div>
     </div>
-    <!-- Status Relawan Field -->
-    <div class="col-sm-4">
-        {!! Form::label('status', 'Status Relawan:') !!}
-        <p>{{ $relawan->status }}</p>
-    </div>
-</div>
-<div class="row">
-    <!-- Wilayah Field -->
-    <div class="col-sm-4">
-        {!! Form::label('id_wilayah', 'Desa:') !!}
-        <p>{{ isset($relawan->desa->nama)?$relawan->desa->nama:""}}</p>
-    </div>
-    <div class="col-sm-4">
-        {!! Form::label('id_wilayah', 'Kecamatan:') !!}
-        <p>{{ isset($relawan->desa->kecamatan->nama)?$relawan->desa->kecamatan->nama:""}}</p>
-    </div>
-
-    <!-- No kta Field -->
-    <div class="col-sm-4">
-        {!! Form::label('no_kta', 'No KTA:') !!}
-        <p>{{ $relawan->no_kta }}</p>
-    </div>
-
-   
-</div>
-<div class="row">
-     <!-- NIK Field -->
-     <div class="col-sm-4">
-        {!! Form::label('nik', 'NIK:') !!}
-        <p>{{ $relawan->nik }}</p>
-    </div>
-    <!-- Jenis Kelamin Field -->
-    <div class="col-sm-4">
-        {!! Form::label('jenis_kelamin', 'Jenis Kelamin:') !!}
-        <p>{{ $relawan->jenis_kelamin}}</p>
-    </div>
-    <!-- Tempat Lahir Field -->
-    <div class="col-sm-4">
-        {!! Form::label('tempat_lahir', 'Tempat Lahir:') !!}
-        <p>{{ $relawan->tempat_lahir}}</p>
-    </div>
-</div>
-<div class="row">
-    <!-- Tanggal Lahir Field -->
-    <div class="col-sm-4">
-        {!! Form::label('tanggal_lahir', 'Tanggal Lahir:') !!}
-        <p>{{ $relawan->tanggal_lahir}}</p>
-    </div>
-    <!-- Status Perkawinan Field -->
-    <div class="col-sm-4">
-        {!! Form::label('status_perkawinan', 'Status Perkawinan:') !!}
-        <p>{{ $relawan->status_perkawinan}}</p>
-    </div>
-
-    <!-- Kontak Field -->
-    <div class="col-sm-4">
-        {!! Form::label('contact', 'Kontak:') !!}
-        <p>{{ $relawan->users->contact}}</p>
-    </div>
-
-    <!-- Alamat Field -->
-    <div class="col-sm-4">
-        {!! Form::label('alamat', 'Alamat:') !!}
-        <p>{{ $relawan->users->alamat}}</p>
     </div>
 </div>
