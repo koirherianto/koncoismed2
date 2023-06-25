d23721219762440eafa233100a14c2cf96a6e574
Zaenab <zaenab.1701@gmail.com>
2023-06-25 20:51:14 +0800

    peta ui blade +

--- resources/views/maps/relawan.blade.php
+++ resources/views/maps/relawan.blade.php
@@ -1,12 +1,43 @@
 @extends('layouts.app')
 @section('content')
-<div class="card">
+{{-- <div class="card">
     <div class="border-white border-left-6 box-shadow-1 rounded">
         <div class="card-content ">
             <div id="map"></div>
         </div>
     </div>
-</div>
+</div> --}}
+<section id="gmaps-basic-maps">
+    <!-- Basic Map -->
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Persebaran Relawan Konco Ismed</h4>
+                    <a class="heading-elements-toggle"><i class="fa fa-ellipsis-v font-medium-3"></i></a>
+                    <div class="heading-elements">
+                        <ul class="list-inline mb-0">
+                            {{-- <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                            <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li> --}}
+                            <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                            {{-- <li><a data-action="close"><i class="ft-x"></i></a></li> --}}
+                        </ul>
+                    </div>
+                </div>
+                <div class="card-content collapse show">
+                    <div class="card-body">
+                        {{-- <p class="card-text">You must define <strong>container ID</strong>, <strong>latitude</strong> and <strong>longitude</strong> of the map's center.</p>
+
+                        <p class="card-text">You also can define <strong>zoom</strong>, <strong>width</strong> and <strong>height</strong>. By default, zoom is 15. Width an height in a CSS class will replace these values.</p>
+
+                        <p class="card-text">Map types are defined in the <strong>mapType</strong> property. Allowed values are: <strong>roadmap</strong> (default), <strong>satellite</strong>, <strong>hybrid</strong> and <strong>terrain</strong>.</p> --}}
+                        <div id="map"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
 @section('scripts')
     <script>
