9a4fbceea12c54479906464a7cf286c43ed697ad
Zaenab <zaenab.1701@gmail.com>
2023-07-23 11:50:42 +0800

    fix map relawan

--- app/Models/Peta.php
+++ app/Models/Peta.php
@@ -8,18 +8,31 @@ use App\Models\Relawan;
 use App\Models\Kecamatan;
 use App\Models\Desa;
 use DB;
+use Auth;
 
 class Peta extends Model
 {
     public function allDataRelawan(){
-        $results = DB::table('relawan')
-        ->join('desa', 'relawan.id_wilayah', '=', 'desa.id')
-        ->join('users', 'relawan.users_id', '=', 'users.id')
-        ->select(DB::raw('users.name as nama, desa.latitude as latitude, 
-            desa.longitude as longitude'))
-        ->get();
 
-        return $results;
+        if((Auth::user()->hasAnyRole(['admin-kandidat-free', 'admin-kandidat-premium','super-admin']))){
+            $results = DB::table('relawan')
+            ->join('desa', 'relawan.id_wilayah', '=', 'desa.id')
+            ->join('users', 'relawan.users_id', '=', 'users.id')
+            ->select(DB::raw('users.name as nama, desa.latitude as latitude, desa.longitude as longitude'))
+            ->get();
+
+            return $results;
+        }else{
+            //untuk relawan
+            $results = DB::table('relawan')
+            ->join('desa', 'relawan.id_wilayah', '=', 'desa.id')
+            ->join('users', 'relawan.users_id', '=', 'users.id')
+            ->select(DB::raw('users.name as nama, desa.latitude as latitude, desa.longitude as longitude'))
+            ->where('relawan_id', Auth::user()->relawan->id)
+            ->get();
+
+            return $results;
+        }
     }
 
 
